0460ab0998337f9dc85d71732f0d4c128ed6d265
ReloadingC <maximkrish@gmail.com>
2023-11-16 18:34:29 +0200

    first commit

--- src/main/java/org/example/AirlinesHillelApplication.java
+++ src/main/java/org/example/AirlinesHillelApplication.java
@@ -3,9 +3,10 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 
 public class AirlinesHillelApplication {
     public static void main(String[] args) {
         SpringApplication.run(AirlinesHillelApplication.class, args);
-        }
     }
+}
